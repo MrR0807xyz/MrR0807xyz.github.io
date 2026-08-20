8b5ab7666bb1d9f641c0782c1293c6fef437139a
MrR0807 <mrr0807@users.noreply.github.com>
2026-08-20 21:11:00 +0100

    chore: configure clean root URL for username.github.io

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mrr0807xyz.github.io',
-  base: '/mrr0807',
   devToolbar: {
     enabled: false,
   },
